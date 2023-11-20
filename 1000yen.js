fef6c6cae2972dc55173f66c65f7d69cb2700e80
ikimiti4408 <91766187+ikimiti4408@users.noreply.github.com>
2023-11-20 22:35:16 +0900

    Initial commit

---
+++ 1000yen.js
@@ -0,0 +1,60 @@
+class FoodData {
+    constructor(id, name, price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
+
+function comp(f1, f2) {
+    if (f1.price < f2.price) return -1;
+    else if (f1.price > f2.price) return 1;
+    return 0;
+}
+
+v = [];
+
+const main = () => {
+    document.getElementById('outputtable').innerHTML = '';
+    let texts = document.getElementById('menua').value;
+    v = texts.split('\n');
+
+    const border = parseInt(document.getElementById('bordernum').value);
+    const allFoodData = [];
+    const output = [];
+    const idFoodData = {};
+
+    for (let i = 0; i < v.length; i++) {
+        const [name, price] = v[i].split(',');
+        if (name.substr(0,1) == '#') continue;
+        const id = i;
+        const fd = new FoodData(id, name, parseInt(price));
+        allFoodData.push(fd);
+        idFoodData[id] = fd;
+    }
+
+    allFoodData.sort(comp);
+    console.log(v);
+    let now = border;
+
+    while (now >= allFoodData[0].price) {
+        const searchData = new FoodData(0, null, now);
+        let ModNum = allFoodData.findIndex(element => element.price > searchData.price);
+        if (ModNum == -1) ModNum = allFoodData.length;
+        const idx = (ModNum > 0 ? Math.floor(Math.random() * 1000000) % ModNum : -1);
+
+        if (idx === -1) break;
+
+        const addData = allFoodData[idx];
+        output.push(addData);
+        now -= addData.price;
+    }
+
+    for (let i = 0; i < output.length; i++) {
+        const fd = idFoodData[output[i].id];
+        console.log(`${fd.name} ${fd.price}`);
+        document.getElementById('outputtable').innerHTML +=  `<tr><td>${i+1}</td><td>${fd.name}</td> <td>${fd.price}円</td></tr>`;
+    }
+};
+
+document.getElementById('runbtn').addEventListener('click', main);
